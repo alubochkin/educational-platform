477950186ab517ff43e3c5c49c3e4465dea02ee0
Aleksandra <aatroshina@gmail.com>
2021-03-03 11:04:23 +0300

    group

---
+++ api/models/Group.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const groupSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  start: { type: Date, default: 0 },
+  finish: { type: Date, default: 0 },
+  status: { type: Boolean, default: false },
+  phaseId: { type: mongoose.Schema.Types.ObjectId, ref: 'Phase', required: true },
+  phaseTitle: { type: String, required: true}
+});
+
+module.exports = mongoose.model('Phase', phaseSchema);
