ed7912425b55fb1c242c4f30990bd9b34bc9d40d
Olga Kolosova <panther28kob@gmail.com>
2021-03-04 10:33:58 +0300

    Change nodemailer

--- api/controllers/nodemailer.js
+++ api/controllers/nodemailer.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer')
 const sendMsg = async (tocken) => {
 
   let transporter = nodemailer.createTransport({
-    host: 'olga-HP-Laptop-17-ca2xxx',
+    host: 'localhost',
     port: 25,
     secure: false,
     ignoreTLS: true
@@ -11,8 +11,8 @@ const sendMsg = async (tocken) => {
 
 
   await transporter.sendMail({
-    from: 'olga@olga-HP-Laptop-17-ca2xxx',
-    to: 'kolb2006puma@mail.ru',
+    from: 'ttt@ttt.ru',
+    to: 'ttt2@ttt2.ru',
     subject: 'Message registration',
     text: 'This message was sent from registration server.'
   }).then((data) => { console.log(data) }).catch((err) => { console.log(err) });
