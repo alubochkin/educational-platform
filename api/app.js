582eb6b07f8440a82c4ce8bdcf1aeb33e2d92a4d
Olga Kolosova <panther28kob@gmail.com>
2021-03-06 21:50:58 +0300

    test app

--- api/app.js
+++ api/app.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,PUT,DELETE,OPTIONS');
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
   }
