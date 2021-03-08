8f25d2f1cb4b864afce7b10439e91db1e6cd4e07
Katya <ekaterina.leo.nikiforova@gmail.com>
2021-03-08 17:18:46 +0300

    started add modules & render modules

--- src/components/admins/AdminSyllabus.js
+++ src/components/admins/AdminSyllabus.js
@@ -1,40 +1,32 @@
-import React, {useState, useEffect } from 'react';
-// import { useHistory } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { TextField, Container, Button, Select, InputLabel } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { getModulesThunk, addModulesThunk } from '../../redux/actions/actionModules';
+import ModuleList from '../modules/ModuleList';
 
 const useStyles = makeStyles({
   container: {
+    width: '50%',
     marginTop: '20px',
     padding: '5px',
     border: 'solid 1px #4253AF',
     borderRadius: '5px',
-  },
-  form: {
-    maxWidth: '80%',
-    margin: '0 auto',
-    display: 'grid',
   },
   formItems: {
     display: 'grid',
     gridTemplateColumns: '1fr 1fr',
     gap: '30px'
   },
-  submit: {
-    marginTop: '50px',
-    maxWidth: 'max-content',
-  }
 });
 function AdminSyllabus() {
   const dispatch = useDispatch();
   const classes = useStyles();
   const { spec } = useSelector((state) => state.specReducer);
-  
+  const { user } = useSelector(state => state.userReducer);
+
   const [syllabus, setSyllabus] = useState({});
   const [isAdding, setAdding] = useState(false);
-  const { user } = useSelector(state => state.userReducer);
 
   useEffect(() => {
     dispatch(getModulesThunk(user._id));
@@ -57,56 +49,59 @@ function AdminSyllabus() {
       setAdding(true);
     }
   }
-  
+
   return (
-    <Container className={classes.container} maxWidth={false}>
-      <h1>Учебная программа</h1>
-      <InputLabel id="demo-controlled-open-select-label">Выберите направление</InputLabel>
-      <div className={classes.formItems}>
-        <Select
-          required
-          name="syllabusSpec"
-          native
-          value={syllabus.syllabusSpec}
-          onChange={handleChange}
+    <div>
+      <Container className={classes.container} maxWidth={false}>
+        <h1>Учебная программа</h1>
+        <InputLabel id="demo-controlled-open-select-label">Выберите направление</InputLabel>
+        <div className={classes.formItems}>
+          <Select
+            required
+            name="syllabusSpec"
+            native
+            value={syllabus.syllabusSpec}
+            onChange={handleChange}
           >
-          <option aria-label="None" value="" />
-          {spec.map((el) => {
-            return (
-              <option key={Math.random()} value={el.title}>{el.title}</option>
-            )
-          })}
-        </Select>
-      </div>
-      <Button
-        onClick={addModuleHandler}
-        variant="outlined"
-        size="large"
-        color="primary"
-        className={classes.submit}
-      >
-        Добавить модуль
-      </Button>
-      {isAdding && <div> <TextField
-        className={classes.textField}
-        required
-        InputLabelProps={{
-          shrink: true,
-        }}
-        onChange={handleChange}
-        type="text"
-        name="moduleTitle" label="Наименование модуля" />
+            <option aria-label="None" value="" />
+            {spec.map((el) => {
+              return (
+                <option key={Math.random()} value={el.title}>{el.title}</option>
+              )
+            })}
+          </Select>
+        </div>
+
+        <ModuleList spec={syllabus.syllabusSpec} />
+
         <Button
-        onClick={addModuleHandler}
-        variant="outlined"
-        size="small"
-        color="primary"
-        className={classes.submit}
-      >
-        ОК
+          onClick={addModuleHandler}
+          variant="outlined"
+          size="large"
+          color="primary"
+        >
+          Добавить модуль
       </Button>
-      </div>}
-    </Container>
+        {isAdding && <div> <TextField
+          className={classes.textField}
+          required
+          InputLabelProps={{
+            shrink: true,
+          }}
+          onChange={handleChange}
+          type="text"
+          name="moduleTitle" label="Наименование модуля" />
+          <Button
+            onClick={addModuleHandler}
+            variant="outlined"
+            size="small"
+            color="primary"
+          >
+            ОК
+      </Button>
+        </div>}
+      </Container>
+    </div>
   );
 }
 export default AdminSyllabus;
---
+++ src/components/modules/ModuleList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import ModuleItem from './ModuleItem';
+
+export default function ModuleList({ spec }) {
+  const { modules } = useSelector(state => state.moduleReducer);
+
+  const moduleList = modules.filter((el) => el.titleSpec === spec);
+
+  return (
+    <div>
+      {moduleList && moduleList.map((el) => {
+        return (
+          <div key={Math.random()}>
+            <ModuleItem module={el} />
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
