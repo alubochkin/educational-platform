ad14e7c7213f43f2b5a152a7f9332387e77ccb72
Olga Kolosova <panther28kob@gmail.com>
2021-03-10 10:07:26 +0300

    Add lesson

--- api/models/Lection.js
+++ api/models/Lection.js
@@ -2,16 +2,14 @@ const mongoose = require('mongoose');
 
 const lectionSchema = new mongoose.Schema({
   groupId: { type: mongoose.Schema.Types.ObjectId, ref: 'Group', required: true },
-  groupTitle: { type: String, required: true },
-  date: { type: Date, ref: 'Date', required: true },
-  link: { type: String, default: null },
   phaseId: { type: mongoose.Schema.Types.ObjectId, ref: 'Phase', required: true },
   phaseTitle: { type: String, default: '' },
-  сode: { type: String, default: '' },
-  presentation: { type: String, default: '' },
-  linksLection: { type: String, default: '' },
-  linksVideo: { type: String, default: '' },
-  linksResours: { type: String, default: '' }
+  schId: { type: mongoose.Schema.Types.ObjectId, ref: 'Schedule', required: true },
+  schTitle: { type: String, default: '' },
+  week: { type: Number, default: 0 },
+  dayWeek: { type: Number, default: 0 },
+  dayWeekText: { type: String, default: '' },
+  task: { type: String, default: '' }
 });
 
 module.exports = mongoose.model('Lection', lectionSchema);
