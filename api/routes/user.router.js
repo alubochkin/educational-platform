c5c6b36425141c1c111f88d26dc634748f39ed91
Olga Kolosova <panther28kob@gmail.com>
2021-03-10 14:00:16 +0300

    Not work avatar

--- api/routes/user.router.js
+++ api/routes/user.router.js
@@ -4,9 +4,9 @@ const User = require('../models/User');
 const router = express.Router();
 
 const avatar = multer({
-  limits: {
-    fileSize: 100000000,
-  },
+  // limits: {
+  //   fileSize: 100000000,
+  // },
   fileFilter(req, file, cb) {
     if (!file.originalname.match(/\.(jpg|png|JPG|PNG|JPEG|jpeg)$/))
       return cb(new Error('This is not a correct format of the file'))
@@ -16,6 +16,7 @@ const avatar = multer({
 
 router.post('/avatar', avatar.single('avatar'), async (req, res) => {
   const { userId } = req.body;
+  console.log('*******', req.file)
   const user = await User.findByIdAndUpdate({ _id: userId }, { $set: { avatar: req.file.buffer } })
   res.json({ id: user.id, avatar: user.avatar })
 }, (err, req, res, next) => res.status(404).json({ error: err }));
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -6,9 +6,46 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import TeacherOfficePage from './TeacherOfficePage';
 import StudentAuth from '../components/AuthStudent/FormAuth';
+import NotificationsNoneOutlinedIcon from '@material-ui/icons/NotificationsNoneOutlined';
+import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined';
+import SchoolOutlinedIcon from '@material-ui/icons/SchoolOutlined';
 
 const useStyles = makeStyles({
-  content: {
+  container_menu: {
+    display: "flex",
+    flexDirection: "column",
+    marginBottom: "20px"
+  },
+  menu: {
+    display: "flex",
+    marginTop: "30px",
+    width: "1200px",
+    margin: "30px auto",
+    textAlign: "center",
+  },
+  boxNew:
+  {
+    display: "flex",
+    flexDirection: "row",
+    maxWidth: "100%"
+  },
+  link: {
+    width: "400px",
+    height: "150px",
+    margin: "0 40px 40px 0",
+    padding: "15px 10px",
+    lineHeight: "40px",
+    fontSize: "24px",
+    textAlign: "center",
+    border: "1px solid",
+    borderRadius: "15px",
+    textDecoration: "none"
+  },
+  logo: {
+    display: "inline-block",
+    verticalAlign: "middle",
+    height: "80px",
+    fontSize: "70px",
   }
 });
 
@@ -24,9 +61,29 @@ const MainPage = () => {
     <React.Fragment>
       <CssBaseline />
       <Container className={classes.content} maxWidth={false}>
-
-        <h2>Главная!</h2>
-        <img src='images/main-page.jpg' alt='danya main-page' />
+        <div className={classes.container_menu}>
+          <img src='images/main-page.jpg' alt='danya main-page' />
+          {user.role === 3 && <div className={classes.menu}>
+            <a className={classes.link} href="/userPage"><span className={classes.logo}>{<PersonOutlineOutlinedIcon />}</span>Личный кабинет</a>
+            <a className={classes.link} href="/notifications"><span className={classes.logo}>{<NotificationsNoneOutlinedIcon />}</span>Оповещения</a>
+            <a className={classes.link} href="/studentoffice"><span className={classes.logo}>{<SchoolOutlinedIcon />}</span>Кампус</a>
+          </div>}
+          {user.role === 2 && <div className={classes.menu}>
+            <a className={classes.link} href="/userPage"><span className={classes.logo}>{<PersonOutlineOutlinedIcon />}</span>Личный кабинет</a>
+            <a className={classes.link} href="/notifications"><span className={classes.logo}>{<NotificationsNoneOutlinedIcon />}</span>Оповещения</a>
+            <a className={classes.link} href="/teacheroffice"><span className={classes.logo}>{<SchoolOutlinedIcon />}</span>Офис</a>
+          </div>}
+          {user.role === 1 && <div className={classes.menu}>
+            <a className={classes.link} href="/adminPage"><span className={classes.logo}>{<PersonOutlineOutlinedIcon />}</span>Личный кабинет</a>
+            <a className={classes.link} href="/notifications"><span className={classes.logo}>{<NotificationsNoneOutlinedIcon />}</span>Оповещения</a>
+            <a className={classes.link} href="/adminOffice"><span className={classes.logo}>{<SchoolOutlinedIcon />}</span>Офис</a>
+          </div>}
+        </div>
+        <div className={classes.boxNew}>
+          <div><h1>Новости Elbrus Coding Bootcamp</h1></div>
+          <div><h2>Встреча выпускников</h2></div>
+          <div><h3>Курс алгоритмов</h3></div>
+        </div>
       </Container>
     </React.Fragment>
 
